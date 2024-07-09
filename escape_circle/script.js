831084f2749c2fe5b1de36be10601200fa98ac14
Saran Pariyar <spariyar05@gmail.com>
2024-07-09 15:16:23 +0545

    Next: getting circle location when it moves horizontally

--- escape_circle/script.js
+++ escape_circle/script.js
@@ -1,6 +1,8 @@
 const startBtn = document.getElementById("start-btn");
 const circle = document.getElementById("circle")
 let mode = false;
+let width = window.innerWidth;
+
 
 startBtn.addEventListener("click", ()=>{
     mode = !mode
@@ -13,6 +15,18 @@ startBtn.addEventListener("click", ()=>{
     // mode = !mode
     // startBtn.innerText = 10
     // console.log('H');
-    
+    // circlePosition = circle.offsetLeft;
+    // while(circlePosition <= width) {
+    //     setTimeout((console.log(circlePosition)), 1000)
+        
+    // }
+    if(startBtn.innerText = "Reset"){
+        // first get the value of place of circle when hover using setInterval and circle.offsetLeft
+        // then we can find when it will touch the left part and make the wall glow green when touched.
+    }
 
 })
+
+function hitWall(){
+
+}
