6fe203a6dcc33109195a1702d324bae152f285ee
Saran Pariyar <spariyar05@gmail.com>
2024-07-10 09:48:39 +0545

    Error: reset text not changing after click

--- escape_circle/script.js
+++ escape_circle/script.js
@@ -1,17 +1,18 @@
 const startBtn = document.getElementById("start-btn");
 const circle = document.getElementById("circle")
-let mode = false;
+// let mode = "Stopped";
 let width = window.innerWidth;
 
 
-startBtn.addEventListener("click", ()=>{
-    mode = !mode
-    circle.classList.toggle("start-animation");
-    circle.classList.toggle("circle-hover");
-    
+function startGame(){
 
-    // !mode ? startBtn.innerText("St") : startBtn.innerText("Stop")
-    mode ? startBtn.innerText = "Reset" : startBtn.innerText = "Start"
+
+    circle.classList.toggle("start-animation");
+    // circle.classList.toggle("circle-hover");
+    // console.log(mode);
+
+    
+    
     // mode = !mode
     // startBtn.innerText = 10
     // console.log('H');
@@ -24,8 +25,12 @@ startBtn.addEventListener("click", ()=>{
         // first get the value of place of circle when hover using setInterval and circle.offsetLeft
         // then we can find when it will touch the left part and make the wall glow green when touched.
     }
+    
 
-})
+}
+
+
+startBtn.addEventListener("click",startGame)
 
 function hitWall(){
 
